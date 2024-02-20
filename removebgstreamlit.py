ca1a12598d49a7cf74f336b625777897cae341dd
ARYAN PARMAR <82685818+xryxn13@users.noreply.github.com>
2024-02-20 10:54:51 +0530

    Update removebgstreamlit.py

--- removebgstreamlit.py
+++ removebgstreamlit.py
@@ -1,25 +1,46 @@
 import streamlit as st
+from rembg import remove
 from PIL import Image
-import numpy as np
+from io import BytesIO
+import base64
 
-# Background removal function
-def remove_background(image):
-    # Implement your background removal algorithm here
-    # Replace the following line with your actual implementation
-    processed_image = np.array(image)  # Placeholder
-    return processed_image
+st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-def main():
-    st.title("Background Removal Tool")
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+st.write("## Remove background from your image")
+st.write(
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
 
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Original Image", use_column_width=True)
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-        if st.button("Remove Background"):
-            processed_image = remove_background(image)
-            st.image(processed_image, caption="Image with Background Removed", use_column_width=True)
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
-if __name__ == "__main__":
-    main()
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    fix_image("./zebra.jpg")
